f4185b98b5f21a5a255da3fe2159e22166f9beae
Mauricio-AFLadeira <mauricioafl001@gmail.com>
2023-03-27 20:12:12 -0300

    prototype

---
+++ src/test/java/org/example/prototype/CarroTest.java
@@ -0,0 +1,19 @@
+package org.example.prototype;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CarroTest {
+    @Test
+    void testClone() throws CloneNotSupportedException {
+        Carro carro = new Carro("Fiat", "siena", 4, 200);
+
+        Carro carroClone = carro.clone();
+        carroClone.setMarca("Chevrolet");
+        carroClone.setNumPortass(2);
+
+        assertEquals("Carro{marca=Fiat, modelo='siena', numero de portas=4, velocidade maxima='200'}", carro.toString());
+        assertEquals("Carro{marca=Chevrolet, modelo='siena', numero de portas=2, velocidade maxima='200'}", carroClone.toString());
+    }
+}
